9b261c6d894c4df8701a8cef974a7851132f285a
pranpran <31861622+pranpran@users.noreply.github.com>
2017-10-12 12:31:30 -0500

    Assignment 4
    Pranathi Golkonda

--- Program Problem 3/Source.cpp
+++ Program Problem 3/Source.cpp
@@ -1,7 +1,7 @@
 /*
 Grace Walzel - 10/5/17 P3
 Program Problem 3
-Create a project assuming that a completely trusthrowrthy and perfect user comes along and will enter a 3 digit number into your program as you ask for it.
+Create a project assuming that a completely trusthrowrthy and perfect user comes along and will enter a 3 digit number into your program as you ask for it. If the first digit of the first number is larger than the second and the second is larger than the first print out descending, similarly if the third is larger than the second and the second is larger than the third print out ascending, if neither potion is true print out neither.
 */
 
 //Libraries
@@ -11,7 +11,7 @@ Create a project assuming that a completely trusthrowrthy and perfect user comes
 //namespaces
 using namespace std; //*
 
-//functions()
+					 //functions()
 void pause() {
 	cout << "Press any key to continue...";
 	while (!_kbhit());
@@ -20,29 +20,32 @@ void pause() {
 }
 // main
 void main() {
-	int someThreeDigitNumber;
-	cout << "Enter a three digit number : ";
-	cin >> someThreeDigitNumber;
-	int A, B, C;
-	
-	A = someThreeDigitNumber / 100 ;
-	
-	B = (someThreeDigitNumber / 10) % 10;
-	
-	C = someThreeDigitNumber % 10;
+	int i = 0; // define loop number
+	while (i < 30) {
+		int someThreeDigitNumber;
+		cout << "Enter a three digit number : ";
+		cin >> someThreeDigitNumber;
+		int A, B, C;
 
-	if (A<B && B<C) { // check to see if ascending
-		
-		cout << "The number is ascending";
-	}
-	else if (A>B && B>C) { // check to see if descending
-		
-		cout << "The number is descending";
-	}
-	else if (A=B=C) { //check to see if neither
-		
-		cout << "The number is neither ascending nor descending" << endl;
+		A = someThreeDigitNumber / 100; //First digit
+
+		B = (someThreeDigitNumber / 10) % 10; //Second digit
+
+		C = someThreeDigitNumber % 10; //Third digit
+
+		if (A < B && B < C) { // check to see if ascending
+
+			cout << someThreeDigitNumber << " is ascending" << endl;
+		}
+		else if (A > B && B > C) { // check to see if descending
+
+			cout << someThreeDigitNumber << " is descending" << endl;
+		}
+		else { //check to see if neither, A=B=C is only one classification of numbers that can be considered niether.
+
+			cout << someThreeDigitNumber << " is neither ascending nor descending" << endl;
+		}
+		i++;
 	}
 	pause(); // pauses to see the displayed text
-
 }
